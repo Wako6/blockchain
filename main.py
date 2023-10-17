eef0a1d582196fa31ef1d3e1321fa2e8f85352eb
Sullivan ADOLPHE <sullivan.adolphe@ipeos.com>
2023-10-17 09:18:15 -0400

    [UPDATE] Improve the blockchain

--- main.py
+++ main.py
@@ -5,72 +5,101 @@ import json
 
 class BlockCode:
 
-    def __init__(self, previous_hash, content):
-        self.previous_hash = previous_hash
-        self.content = content
-        self.index = "N/A"
-        self.datetime = str(datetime.datetime.now())
-        self.hash = self.hash()
+    def __init__(self, previous_hash, content, index="N/A", *args, **kwargs):
+        self._previous_hash = previous_hash
+        self._content = content
+        self._index = index
+        self._datetime = str(datetime.datetime.now())
+        self._hash = self.get_hash()
 
     def __str__(self):
-        return f"index: {self.index}\n" \
-               f"datetime: {self.datetime}\n" \
-               f"previous_hash: {self.previous_hash}\n" \
-               f"hash: {self.hash}\n" \
-               f"content: {self.content}"
+        return f"index: {self._index}\n" \
+               f"datetime: {self._datetime}\n" \
+               f"previous_hash: {self._previous_hash}\n" \
+               f"hash: {self._hash}\n" \
+               f"content: {self._content}"
 
-    def hash(self):
-        block_data = {
-            "index": self.index,
-            "datetime": self.datetime,
-            "previous_hash": self.previous_hash,
-            "content": self.content
+    def __repr__(self):
+        return self.__str__()
+
+    def get_hash(self):
+        data = {
+            "index": self._index,
+            "datetime": self._datetime,
+            "previous_hash": self._previous_hash,
+            "content": self._content
         }
-        encoded_block = json.dumps(block_data, sort_keys=True).encode()
+
+        encoded_block = json.dumps(data, sort_keys=True).encode()
 
         return hashlib.sha256(encoded_block).hexdigest()
 
+    @property
+    def previous_hash(self):
+        return self._previous_hash
+
     @previous_hash.setter
     def previous_hash(self, previous_hash):
-        self.previous_hash = previous_hash
-        self.hash = self.hash()
+        self._previous_hash = previous_hash
+        self._hash = self.get_hash()
+
+    @property
+    def content(self):
+        return self._content
 
     @content.setter
     def content(self, content):
-        self.content = content
-        self.hash = self.hash()
+        self._content = content
+        self._hash = self.get_hash()
+
+    @property
+    def index(self):
+        return self._index
 
     @index.setter
     def index(self, index):
-        self.index = index
-        self.hash = self.hash()
+        self._index = index
+        self._hash = self.get_hash()
+
+    @property
+    def datetime(self):
+        return self._datetime
 
     @datetime.setter
     def datetime(self, datetime):
-        self.datetime = datetime
-        self.hash = self.hash()
+        self._datetime = datetime
+        self._hash = self.get_hash()
+
+    @property
+    def hash(self):
+        return self._hash
 
     @hash.setter
     def hash(self, hash):
-        self.hash = self.hash()
+        self._hash = self.get_hash()
 
 
 class TextBlock(BlockCode):
 
-    def __init__(self, previous_block_hash, text, **kwargs):
-        super().__init__(self, previous_block_hash, text, **kwargs)
+    def __init__(self, previous_block_hash, text, *args, **kwargs):
+        super().__init__(previous_block_hash, text, *args, **kwargs)
 
 
 class BlockChain:
 
     def __init__(self, genesis_message="Genesis Block"):
         self.chain = []
-        self.chain.append(TextBlock("0", genesis_message, 1))
+        genesis_block = BlockCode("0", genesis_message, index=1)
+        self.chain.append(genesis_block)
 
-    def add_block(self, block: BlockCode):
-        block.index = len(self.chain) + 1
+    def add_block(self, content):
+        block = BlockCode(self.chain[-1].hash,
+                          content,
+                          index=len(self.chain) + 1)
         self.chain.append(block)
 
     def display(self):
-        for block in self.chain:
+        for i, block in enumerate(self.chain):
+            if i != 0:
+                print()
             print(block)
---
+++ test.py
@@ -0,0 +1,7 @@
+from main import BlockChain
+
+bc = BlockChain()
+
+bc.add_block("This block is my first one")
+
+bc.display()
